4b60db5bc20787efd547d7206899f64f6f342cf6
Kan-Ru Chen <kanru@kanru.info>
2009-10-24 23:54:29 +0800

    Move customize to separate file.

--- kanru/50wl.el
+++ kanru/50wl.el
@@ -2,6 +2,17 @@
 (require 'wl-spam)
 
 (setq
+ wl-default-folder ".inbox"
+ wl-draft-folder ".draft"
+ wl-trash-folder ".trash"
+ wl-queue-folder ".queue"
+ wl-spam-folder ".spam"
+ wl-refile-default-from-folder ".from"
+ wl-draft-send-mail-function (quote sendmail-send-it)
+ wl-from "Kan-Ru Chen <kanru@anar.kanru.info>"
+ wl-spam-undecided-folder-list (quote (".unsure-bogofilter"))
+ wl-summary-width nil
+ wl-user-mail-address-list (quote ("koster@debian.org.tw" "ckanru@gmail.com"))
  wl-message-ignored-field-list '("^.*:")
  wl-message-visible-field-list
   '("^\\(To\\|Cc\\):"
