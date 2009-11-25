8145c65acb4efc8aa93bead9e31731974c499143
Kan-Ru Chen <kanru@anar.kanru.info>
2009-11-25 14:12:36 +0800

    Add more mail address and mailing lists.

--- kanru/50wl.el
+++ kanru/50wl.el
@@ -15,8 +15,6 @@
 (add-hook 'mime-view-mode-hook
 	  (lambda ()
 	    (local-set-key "f" 'browse-url)))
-;(define-key wl-summary-mode-map "q"    'wl-summary-suspend)
-;(define-key wl-summary-mode-map "Q"    'wl-summary-exit)
 
 (setq
  elmo-localdir-folder-path "~/Maildir"
@@ -29,32 +27,43 @@
  wl-spam-folder ".spam"
  wl-refile-default-from-folder ".from"
  wl-draft-send-mail-function (quote sendmail-send-it)
- wl-from "Kan-Ru Chen <kanru@anar.kanru.info>"
+ wl-from "Kan-Ru Chen <koster@debian.org.tw>"
  wl-spam-undecided-folder-list (quote (".unsure-bogofilter"))
  wl-summary-width nil
  wl-draft-always-delete-myself t
  wl-draft-delete-myself-from-bcc-fcc t
  wl-draft-preview-attributes-list '((mail recipients envelope-from)
                                     (news newsgroups nntp-posting-server nntp-posting-port))
- wl-user-mail-address-list (quote ("koster@debian.org.tw" "ckanru@gmail.com" "kanru@kanru.info"))
+ wl-user-mail-address-list '("koster@debian.org.tw"
+                             "ckanru@gmail.com"
+                             "kanru@kanru.info"
+                             "kanru@anar.kanru.info"
+                             "kanru.96@stu.csie.ncnu.edu.tw")
  wl-generate-mailer-string-function 'wl-generate-user-agent-string-1
  wl-message-ignored-field-list '("^.*:")
- wl-message-visible-field-list
-  '("^\\(To\\|Cc\\):"
-    
-    "^Subject:"
-    "^\\(From\\|Reply-To\\):"
-    "^Organization:"
-    "^Message-Id:"
-    "^\\(Posted\\|Date\\):"
-    )
-  wl-message-sort-field-list
-  '("^From"
-    
-    "^Organization:"
-    "^X-Attribution:"
-    "^Subject"
-     "^Date"
-     "^To"
-     "^Cc")
-  )
+ wl-message-visible-field-list '("^\\(To\\|Cc\\):"
+                                 "^Subject:"
+                                 "^\\(From\\|Reply-To\\):"
+                                 "^Organization:"
+                                 "^Message-Id:"
+                                 "^\\(Posted\\|Date\\):"
+                                 )
+ wl-message-sort-field-list '("^From"
+                              "^Organization:"
+                              "^X-Attribution:"
+                              "^Subject"
+                              "^Date"
+                              "^To"
+                              "^Cc")
+ )
+
+(setq
+        wl-subscribed-mailing-list
+        '("hackingthursday@googlegroups.com"
+          "chewing-user@googlegroups.com"
+          "chewing-devel@googlegroups.com"
+          "members@debian.org.tw")
+        wl-insert-mail-followup-to t
+        wl-insert-mail-reply-to t
+        wl-forward-subject-prefix "Fwd:"
+        )
