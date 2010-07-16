5723b95127eefa1a54be46518deb7f8808a32786
Kan-Ru Chen <kanru@0xlab.org>
2010-07-16 23:18:19 +0800

    Disable jabber mode

--- kanru/50jabber.el
+++
@@ -1,9 +0,0 @@
-(require 'jabber-autoloads)
-
-(setq jabber-account-list
-      '(("ckanru@gmail.com"
-         ;;   (:password . nil) or (:password . "your-pass")
-         (:network-server . "talk.google.com")
-         (:port . 443)
-         (:connection-type . ssl))
-        ))
